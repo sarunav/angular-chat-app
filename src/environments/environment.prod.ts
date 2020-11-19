2e40e58f114fec97f0a0f9c5cc70efaddd1313f3
sarunav <sarun@interface.ai>
2020-11-20 01:26:36 +0530

    Prod env added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAhrH_dkPZ-oSPcPLHfHL0602NHBS2sBSQ',
+    authDomain: 'chat-app-8ccbe.firebaseapp.com',
+    databaseURL: 'https://chat-app-8ccbe.firebaseio.com',
+    projectId: 'chat-app-8ccbe',
+    storageBucket: 'chat-app-8ccbe.appspot.com',
+    messagingSenderId: '933073629316',
+    appId: '1:933073629316:web:ab2b78bc17f3049ad312d1'
+  }
 };
